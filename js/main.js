a59d82369788fa88e631cbb551b80736aff77f56
c.dehondt <c.dehondt@live.com>
2017-06-16 14:39:25 +0200

    all add element for to project(animation site)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 function abonner(){
-  confirm("voulez vous confirmer?");
+  confirm("voulez vous confirmer votre inscription?");
+}
+
+
+function on() {
+    var x= document.getElementById("imgArticle").alt;
+    document.getElementById("demo").innerHTML = x;
+}
+function date(id){
+  var d = new Date();
+  var d = new Date(milliseconds);
+  var d = new Date(dateString);
+  var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
 }
